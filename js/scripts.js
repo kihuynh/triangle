1659c9b61d5994a83b6aa9e663c5e0b948e4ab3f
Hannah Lee and Kimberly Huynh <hannah@>
2018-01-11 15:45:52 -0800

    add triangle rules for not a triangle, triangle, and isosoceles

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+  var side1 = parseInt(prompt("Length one:"));
+  var side2 = parseInt(prompt("Length two:"));
+  var side3 = parseInt(prompt("Length three:"));
+
+  if (side1 + side2 <= side3) {
+    $("#not-triangle").show();
+  } else if (side2 + side3 <= side1) {
+    $("#not-triangle").show();
+  } else if (side1 + side3 <= side2) {
+    $("#not-triangle").show();
+  }
+
+
+
+
+    else if (side1 === side2 && side2 === side3 && side1 === side3) {
+    $("#equilateral").show();
+    }
+
+    else if (side1 != side2 && side1 === side3) {
+    $("#isosceles").show();
+    }
+
+    else if (side1 === side2 && side2 != side3) {
+    $("#isosceles").show();
+    }
+
+    else if (side1 === side2 && side1 != side3) {
+    $("#isosceles").show();
+    }
+
+
+
+
+
+
+
+
+});
